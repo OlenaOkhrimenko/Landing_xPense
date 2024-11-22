5d42b44735437c8042ade2159cd86cab2a54bc12
Ім'я користувача <>
2024-11-22 14:18:06 +0200

    first commit

---
+++ js/mobile_menu.js
@@ -0,0 +1,7 @@
+let menuButton = document.querySelector('.menu_button');
+let nav = document.querySelector('.nav');
+
+menuButton.addEventListener('click', function () {
+	nav.classList.toggle('active');
+	
+});
